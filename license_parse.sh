799483ffe34ed61ff5a9f593f3e6593341dbcbd4
Hirofumi Watanabe <hirofumi01.watanabe@g.softbank.co.jp>
2019-08-06 20:14:59 +0900

    initial

---
+++ license_parse.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+find $PWD -type f -name package.xml | while read file;do
+# packageの名称を取得
+name_xml=`echo "cat /package/name" | xmllint --shell ${file}`
+# licenseを取得
+license_xml=`echo "cat /package/license" | xmllint --shell ${file}`
+
+
+# 全て取得した場合、ループを抜ける
+if [  "${name_xml}" = "/ > / > " ]; then
+    break
+fi
+
+# 取得内容を整形
+name=`echo ${name_xml} | sed -e "s/^.*<name.*>\(.*\)<\/name>.*$/\1/"`
+license=`echo ${license_xml} | sed -e "s/^.*<license.*>\(.*\)<\/license>.*$/\1/"`
+
+# 取得結果を表示
+# if [ "${license}" != "BSD" ]; then
+# if [ "${license}" != "BSD" -a "${license}" != "MIT" ]    ; then
+    # if  $(echo ${license} | grep -v Apache > /dev/null) ; then
+        echo "${name}, ${license}"
+    # fi
+# fi
+done
