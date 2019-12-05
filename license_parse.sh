98c3d92ebb307fcae9e16a5ce8eee581c91b1073
Hirofumi Watanabe <hirofumi01.watanabe@g.softbank.co.jp>
2019-12-05 11:50:23 +0900

    get url

--- license_parse.sh
+++ license_parse.sh
@@ -4,6 +4,7 @@ find $PWD -type f -name package.xml | while read file;do
 name_xml=`echo "cat /package/name" | xmllint --shell ${file}`
 # licenseを取得
 license_xml=`echo "cat /package/license" | xmllint --shell ${file}`
+url_xml=`echo "cat /package/url" | xmllint --shell ${file} | sed -e "s/^.*<url.*>\(.*\)<\/url>.*$/\1/" |grep http |tr "\n" ","`
 
 
 # 全て取得した場合、ループを抜ける
@@ -14,12 +15,13 @@ fi
 # 取得内容を整形
 name=`echo ${name_xml} | sed -e "s/^.*<name.*>\(.*\)<\/name>.*$/\1/"`
 license=`echo ${license_xml} | sed -e "s/^.*<license.*>\(.*\)<\/license>.*$/\1/"`
+url=`echo ${url_xml}`
 
 # 取得結果を表示
 # if [ "${license}" != "BSD" ]; then
 # if [ "${license}" != "BSD" -a "${license}" != "MIT" ]    ; then
     # if  $(echo ${license} | grep -v Apache > /dev/null) ; then
-        echo "${name}, ${license}"
+        echo "${name}, ${license}, ${url}"
     # fi
 # fi
 done
